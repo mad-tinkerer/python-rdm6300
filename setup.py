04b3f06a1e5d05bb8ffa2ad599a29231cff8f52f
mad-tinkerer <54571278+mad-tinkerer@users.noreply.github.com>
2019-12-27 00:01:52 +0100

    Circleci (#1)
    Make sure that our project can be built using circle CI.

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
+#!/usr/bin/env python
+
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-# TODO: add dependency on pyserial
 setuptools.setup(
     name="em4100-mad-tinkerer",
-    version="0.0.1",
+    version="0.1.0",
     author="The Mad Thinkerer Me",
     author_email="mad.tiknerer.me@gmail.com",
     description="EM4100 rfid reader",
     long_description=long_description,
+    install_requires=[
+        "pyserial",
+    ],
+    tests_require=[
+        "pyserial",
+        "mock",
+    ],
     long_description_content_type="text/markdown",
     url="https://github.com/mad-tinkerer/python-em4100",
     packages=setuptools.find_packages(),
