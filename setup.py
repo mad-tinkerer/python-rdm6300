543342aa85ca19cf149544d0d478f4b73e17db5d
The Mad Tinkerer Me <mad.tinkerer.me@gmail.com>
2019-12-26 23:53:48 +0100

    Rely on setup.py for testing

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
+#!/usr/bin/env python
+
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-# TODO: add dependency on pyserial
 setuptools.setup(
     name="em4100-mad-tinkerer",
-    version="0.0.1",
+    version="0.1.0",
     author="The Mad Thinkerer Me",
     author_email="mad.tiknerer.me@gmail.com",
     description="EM4100 rfid reader",
     long_description=long_description,
-    install_requires=[
+    install_require=[
         "pyserial",
+    ],
+    tests_requires=[
+        "mock",
     ],
     long_description_content_type="text/markdown",
     url="https://github.com/mad-tinkerer/python-em4100",
